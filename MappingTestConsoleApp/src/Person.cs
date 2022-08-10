32179a45a076300bd811e5cbdce8b0edc93b0fd4
Irme Groothuis <irgr@cellavision.se>
2022-08-10 10:29:03 +0200

    Add project files.

---
+++ MappingTestConsoleApp/src/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mapster;
+
+namespace MappingTesting.src
+{
+    [AdaptTo("[name]Dto"), GenerateMapper]
+    public class Person
+    {
+        public int? age { get; set; }
+        public string? name { get; set; }
+        public string? hobby { get; set; }
+        public int? id { get; set; } 
+        public string? job { get; set; }
+
+    }
+}
